07aa8e641beac691aa8d05ebf9260998d97e8d02
Ali <Alimuhammadiii@outlook.com>
2025-05-19 08:05:52 +0330

    add filter todo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-// import { useState } from "react";
 
 export const Route = createRootRoute({
   component: Root,
@@ -12,20 +11,11 @@ export const Route = createRootRoute({
 });
 
 function Root() {
-  // const [isOpen, setIsOpen] = useState(true);
   return (
     <>
-      <div className="h-full w-full flex">
-        {/* <div
-          className={`bg-gray-800 ${isOpen ? "w-[350px]" : "w-[50px]"} transition-all ease-in-out duration-300 text-white`}
-        >
-          <p>SideBar</p>
-          <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "close" : "open"}</button>
-        </div> */}
-        <div className="bg-[url(assets/cover1.jpg)] bg-cover flex flex-col p-4 gap-4 grow">
-          {" "}
-          <Outlet />
-        </div>
+      <div className="bg-[url(assets/cover1.jpg)] bg-cover flex flex-col p-4 gap-4 grow h-full">
+        {" "}
+        <Outlet />
       </div>
 
       <TanStackRouterDevtools position="bottom-right" />
